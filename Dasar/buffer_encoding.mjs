70d1d9bab3c86a792ce1f5cf40712ded911d328a
M Rifqi <muhamadrifqi.id@gmail.com>
2024-06-30 10:04:11 +0700

    buffer encoding

---
+++ Dasar/buffer_encoding.mjs
@@ -0,0 +1,7 @@
+const buffer = Buffer.from("Muhamad Rifqi", "utf8")
+
+console.log(buffer)
+console.log(buffer.toString())
+console.log(buffer.toString("base64"))
+console.log(buffer.toString("hex"))
+console.log(buffer.toString("base64url"))
